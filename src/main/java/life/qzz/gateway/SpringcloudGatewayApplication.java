bd5c7540528ebc6bceaa375041ede1b06fe8e641
qzz <qzzsunly@163.com>
2017-12-19 21:11:01 +0800

    zipkin

---
+++ src/main/java/life/qzz/gateway/SpringcloudGatewayApplication.java
@@ -0,0 +1,14 @@
+package life.qzz.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class SpringcloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudGatewayApplication.class, args);
+	}
+}
