38b809e3b355bda0574974d5b2b29fc54fdb6406
qzz <qzzsunly@163.com>
2017-12-20 17:10:17 +0800

    eureka

--- src/main/java/life/qzz/gateway/LoggerFilter.java
+++ src/main/java/life/qzz/gateway/LoggerFilter.java
@@ -1,11 +1,13 @@
 package life.qzz.gateway;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Component;
 
-import com.netflix.zuul.ZuulFilter;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <pre>
@@ -18,12 +20,12 @@ public class LoggerFilter extends ZuulFilter{
     private Tracer tracer;
     @Override
     public String filterType() {
-        return FilterConstants.POST_TYPE;
+        return FilterConstants.PRE_TYPE;
     }
 
     @Override
     public int filterOrder() {
-        return 900;
+        return 0;
     }
 
     @Override
@@ -33,8 +35,36 @@ public class LoggerFilter extends ZuulFilter{
 
     @Override
     public Object run() {
-        tracer.addTag("operator", "qzz");
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        String ip = getIpAddr(requestContext.getRequest());
+        tracer.addTag("ip", ip);
         System.out.println(tracer.getCurrentSpan().traceIdString());
         return null;
+    }
+
+    /**
+     * 获取Ip地址
+     * @param request
+     * @return
+     */
+    public  String getIpAddr(HttpServletRequest request){
+
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 }
